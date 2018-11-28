6be353890eba2b1607e0cd24e01137d93d8e6804
Olha Danylova <olga@themidnightcoders.com>
2018-11-28 17:59:32 +0200

    Package-iOS added

---
+++ SwiftSDK/iOS/SwiftSDK.framework/Headers/SwiftSDK_iOS.h
@@ -0,0 +1,18 @@
+//
+//  SwiftSDK_iOS.h
+//  SwiftSDK-iOS
+//
+//  Created by Olha Danylova on 11/28/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSDK_iOS.
+FOUNDATION_EXPORT double SwiftSDK_iOSVersionNumber;
+
+//! Project version string for SwiftSDK_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSDK_iOS/PublicHeader.h>
+
+
