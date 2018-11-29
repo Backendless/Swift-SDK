40a755f2f5fa72f8aac278916e6899855e9cea0f
Olha Danylova <olga@themidnightcoders.com>
2018-11-29 08:32:04 +0200

    Podfiles added

---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '8.0'
+
+target 'SwiftSDK-iOS' do
+  use_frameworks!
+  pod 'Alamofire'
+
+  target 'SwiftSDK-iOSTests' do
+    inherit! :search_paths
+    pod 'Alamofire'
+  end
+
+end
--- Source/MyClass.swift
+++ Source/MyClass.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import Alamofire
 
 open class MyClass: NSObject {
 
@@ -7,5 +8,12 @@ open class MyClass: NSObject {
     
     open func stringMethod(_ str: String?) -> String? {
         return str
+    }
+    
+    open func alamofireMethod() {
+        let url = "http://api.backendless.com/A9C02398-D4D5-AED3-FF4A-635C13AB5F00/A2FDA7CA-A94C-F233-FFB9-554304C06400/data/Person/first"
+        Alamofire.request(url).responseJSON(completionHandler: { response in
+            print(response)
+        })
     }
 }
